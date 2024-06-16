f34f905da29c803649fe90c7d1f580be6e02d1f5
SantLuzz <luiz.santos1749@outlook.com>
2024-06-16 12:02:40 -0400

    Alterações do Frontend - criando autenticacao

---
+++ Dima.Web/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Dima.Web;
+using MudBlazor.Services;
+using Dima.Web.Security;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CookieHandler>();
+builder.Services.AddMudServices();
+
+//Microsoft.Extensions.Http use esse para requisição http
+builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
+{
+    opt.BaseAddress = new Uri(Configuration.BackendUrl);
+}).AddHttpMessageHandler<CookieHandler>();
+
+await builder.Build().RunAsync();
